85c3496673411dc9a1f2b5d41d168d3e62f55a94
Nikita S <nikita.subbotin@itomy.ch>
2019-02-21 16:26:59 +0200

    Added delete item function

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,41 +1,56 @@
-import React from "react";
-import { View, StyleSheet, Image, TouchableOpacity, Text } from "react-native";
+import React, {Component} from "react";
+import {View, StyleSheet, Image, TouchableOpacity, Text} from "react-native";
+import {connect} from "react-redux";
 import Icon from 'react-native-vector-icons/Ionicons';
+import {deletePlace} from "../../store/actions";
 
-const PlaceDetailScreen = props => {
-  return (
-      <View style={styles.container}>
-          <View>
-              <Image source={props.selectedPlace.image} style={styles.placeImage} />
-              <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
-          </View>
-        <View style={styles.deleteButton}>
-            <TouchableOpacity onPress={props.onItemDeleted}>
-              <View>
-                <Icon size={30} name="ios-trash" color="red" />
-              </View>
-            </TouchableOpacity>
-        </View>
-      </View>
-  );
-};
+class PlaceDetailScreen extends Component {
+    placeDeletedHandler = () => {
+        this.props.onDeletePlace(this.props.selectedPlace.key);
+        this.props.navigator.pop();
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View>
+                    <Image source={this.props.selectedPlace.image} style={styles.placeImage}/>
+                    <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
+                </View>
+                <View style={styles.deleteButton}>
+                    <TouchableOpacity onPress={this.placeDeletedHandler}>
+                        <View>
+                            <Icon size={30} name="ios-trash" color="red"/>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
-  container: {
-    margin: 32
-  },
-  placeImage: {
-    height: 200,
-    width: "100%"
-  },
-  placeName: {
-    fontWeight: "bold",
-    textAlign: "center",
-    fontSize: 28
-  },
-  deleteButton: {
-    alignItems: "center"
-  }
+    container: {
+        margin: 32
+    },
+    placeImage: {
+        height: 200,
+        width: "100%"
+    },
+    placeName: {
+        fontWeight: "bold",
+        textAlign: "center",
+        fontSize: 28
+    },
+    deleteButton: {
+        alignItems: "center"
+    }
 });
 
-export default PlaceDetailScreen;
+const mapDispatchToProps = dispatch => {
+    return {
+        onDeletePlace: (key) => dispatch(deletePlace(key))
+    }
+};
+
+export default connect(null, mapDispatchToProps)(PlaceDetailScreen);
