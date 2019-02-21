110b6a71d24b716a411cd55e8519cee4fe4a9f3f
Nikita S <nikita.subbotin@itomy.ch>
2019-02-21 13:12:31 +0200

    Created startMainTabs function

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { Navigation } from 'react-native-navigation';
 import AuthScreen from "./src/screens/Auth/AuthScreen";
+import FindPlaceScreen from "./src/screens/FindPlaceTab/FindPlaceTab";
+import SharePlaceScreen from "./src/screens/SharePlace/SharePlace";
 
 // Register screens
 
 Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
+Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen);
+Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen);
 
 // Start a App
 
 Navigation.startSingleScreenApp({
-   screen: {
-     screen: "awesome-places.AuthScreen",
-     title:"Login"
-   }
+    screen: {
+        screen: "awesome-places.AuthScreen",
+        title: "Login"
+    },
 });
