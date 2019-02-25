ffdfd4c29bdfec5ca22379e7b9c81608285e6db0
Nikita S <nikita.subbotin@itomy.ch>
2019-02-25 09:06:03 +0200

    Install prettier

--- src/components/UI/DefaultInput/DefaultInput.js
+++ src/components/UI/DefaultInput/DefaultInput.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React from "react";
 import { TextInput, StyleSheet } from "react-native";
 
-const DefaultInput = (props) => {
-    return (
-        <TextInput
-            {...props}
-            style={[styles.input, props.style]}
-        />
-    );
+const DefaultInput = props => {
+  return <TextInput {...props} style={[styles.input, props.style]} />;
 };
 
 const styles = StyleSheet.create({
-    input: {
-        width: "100%",
-        borderWidth: 1,
-        borderColor: "#eee",
-        borderRadius: 4,
-        padding: 5,
-        margin: 8
-    }
+  input: {
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#eee",
+    borderRadius: 4,
+    padding: 5,
+    margin: 8
+  }
 });
 
 export default DefaultInput;
