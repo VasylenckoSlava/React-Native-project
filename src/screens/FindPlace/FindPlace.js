2f57b9289534996194c1475f31439905426cb25f
Nikita S <nikita.subbotin@itomy.ch>
2019-02-25 17:38:00 +0200

    Added the  button to find place screen. Styled it

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,53 +1,98 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { View, TouchableOpacity, StyleSheet, Text } from "react-native";
+import { connect } from "react-redux";
 import PlaceList from "../../components/PlaceList/PlaceList";
 
 class FindPlaceScreen extends Component {
-    static navigatorStyle ={
-        navBarButtonColor: "orange"
-    };
-    constructor(props){
-        super(props);
-        this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
-    }
+  static navigatorStyle = {
+    navBarButtonColor: "orange"
+  };
 
-    onNavigatorEvent = (event) => {
-        if(event.type === "NavBarButtonPress"){
-            if(event.id === "sideDrawerToggle") {
-                this.props.navigator.toggleDrawer({
-                    side: "left"
-                })
-            }
-        }
-    };
+  state = {
+    placesLoaded: false
+  };
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+  }
 
-    itemSelectedHandler = key => {
-        const selPlace = this.props.places.find(place => {
-            return place.key === key;
+  onNavigatorEvent = event => {
+    if (event.type === "NavBarButtonPress") {
+      if (event.id === "sideDrawerToggle") {
+        this.props.navigator.toggleDrawer({
+          side: "left"
         });
-        this.props.navigator.push({
-            screen: "awesome-places.PlaceDetailScreen",
-            title: selPlace.name,
-            passProps: {
-              selectedPlace: selPlace
-            }
-        })
-    };
-
-    render() {
-        return (
-            <View>
-                <PlaceList places={this.props.places} onItemSelected={this.itemSelectedHandler} />
-            </View>
-        );
+      }
     }
+  };
+
+  placesSearchHandler = () => {
+    this.setState({
+      placesLoaded: true
+    });
+  };
+
+  itemSelectedHandler = key => {
+    const selPlace = this.props.places.find(place => {
+      return place.key === key;
+    });
+    this.props.navigator.push({
+      screen: "awesome-places.PlaceDetailScreen",
+      title: selPlace.name,
+      passProps: {
+        selectedPlace: selPlace
+      }
+    });
+  };
+
+  render() {
+    let content = (
+      <TouchableOpacity onPress={this.placesSearchHandler}>
+        <View style={styles.searchButton}>
+          <Text style={styles.searchButtonText}>Find Places</Text>
+        </View>
+      </TouchableOpacity>
+    );
+    if (this.state.placesLoaded) {
+      content = (
+        <PlaceList
+          places={this.props.places}
+          onItemSelected={this.itemSelectedHandler}
+        />
+      );
+    }
+
+    return (
+      <View style={this.state.placesLoaded ? null : styles.buttonContainer}>
+        {content}
+      </View>
+    );
+  }
 }
 
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  searchButton: {
+    borderColor: "orange",
+    borderWidth: 3,
+    borderRadius: 50,
+    padding: 20
+  },
+  searchButtonText: {
+    color: "orange",
+    fontWeight: "bold",
+    fontSize: 26
+  }
+});
+
 const mapStateToProps = state => {
-    return {
-        places: state.places.places
-    }
+  return {
+    places: state.places.places
+  };
 };
 
 export default connect(mapStateToProps)(FindPlaceScreen);
