aaa653e3681547cffbf40e0a21f78370d371051d
Nikita S <nikita.subbotin@itomy.ch>
2019-02-28 18:52:17 +0200

    Ctreated reducers and action to the redux

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,34 +1,49 @@
 import { ADD_PLACE, DELETE_PLACE } from "./actionTypes";
+import { uiStartLoading, uiStopLoading } from "./ui";
 
 export const addPlace = (placeName, location, image) => {
   return dispatch => {
-    // const placeData = {
-    //   name: placeName,
-    //   location: location
-    // };
-fetch("https://us-central1-mytestproject-1551194381995.cloudfunctions.net/storeImage", {
-    method: "POST",
-    body: JSON.stringify({
-        image: image.base64
-       })
-     })
-    .catch(err => console.log(err))
-        .catch(err => console.log(err))
-        .then(res => res.json())
-        .then(parsedRes => {
-          console.log(parsedRes)
-
-    // fetch("https://mytestproject-1551194381995.firebaseio.com/places.json", {
-    //   method: "POST",
-    //   body: JSON.stringify(placeData)
-    // })
-    //   .catch(err => console.log(err))
-    //   .then(res => res.json())
-    //   .then(parsedRes => {
-    //     console.log(parsedRes);
-    //   });
-  });
-};
+    dispatch(uiStartLoading());
+    fetch(
+      "https://us-central1-mytestproject-1551194381995.cloudfunctions.net/storeImage",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          image: image.base64
+        })
+      }
+    )
+      .catch(error => {
+        console.log(error);
+        alert("Something went wrong! Try again");
+        dispatch(uiStopLoading());
+      })
+      .then(res => res.json())
+      .then(parsedRes => {
+        const placeData = {
+          name: placeName,
+          location: location,
+          image: parsedRes.imageUrl
+        };
+        return fetch(
+          "https://mytestproject-1551194381995.firebaseio.com/places.json",
+          {
+            method: "POST",
+            body: JSON.stringify(placeData)
+          }
+        );
+      })
+      .catch(error => {
+        console.log(error);
+        alert("Something went wrong! Try again");
+        dispatch(uiStopLoading());
+      })
+      .then(res => res.json())
+      .then(parsedRes => {
+        console.log(parsedRes);
+        dispatch(uiStopLoading());
+      });
+  };
 };
 
 export const deletePlace = key => {
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,17 @@
 import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import placesReducer from "./reducers/places";
+import uiReducer from "./reducers/ui";
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
-  places: placesReducer
+  places: placesReducer,
+  ui: uiReducer
 });
 
 let composeEnhancers = compose;
 
-if(__DEV__) {
-    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+if (__DEV__) {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 }
 
 const configureStore = () => {
