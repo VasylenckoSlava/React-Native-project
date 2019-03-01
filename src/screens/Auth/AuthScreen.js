28a9107a411d92f880d72c39964c041935820d49
Nikita S <nikita.subbotin@itomy.ch>
2019-03-01 11:57:57 +0200

    Enabling firebase authentication. Using the result authentication. Create auth ac tion.

--- src/screens/Auth/AuthScreen.js
+++ src/screens/Auth/AuthScreen.js
@@ -7,9 +7,9 @@ import {
   Dimensions,
   KeyboardAvoidingView,
   Keyboard,
-  TouchableWithoutFeedback
+  TouchableWithoutFeedback,
+  ActivityIndicator
 } from "react-native";
-import startMainTabs from "../MainTabs/startMainTabs";
 import DefaultInput from "../../components/UI/DefaultInput/DefaultInput";
 import HeadingText from "../../components/UI/HeadingText/HeadingText";
 import MainText from "../../components/UI/MainText/MainText";
@@ -79,7 +79,6 @@ class AuthScreen extends Component {
       password: this.state.controls.password.value
     };
     this.props.onLogin(authData);
-    startMainTabs();
   };
 
   updateInputState = (key, value) => {
@@ -131,6 +130,26 @@ class AuthScreen extends Component {
   render() {
     let headingText = null;
     let confirmPasswordControl = null;
+    let submitButton = (
+      <ButtonWithBackground
+        color="#29aaf4"
+        onPress={this.loginHandler}
+        disabled={
+          (!this.state.controls.confirmPassword.valid &&
+            this.state.authMode === "signup") ||
+          !this.state.controls.email.valid ||
+          !this.state.controls.password.valid
+        }
+      >
+        Submit
+      </ButtonWithBackground>
+    );
+
+    if (this.props.isLoading) {
+      {
+        submitButton = <ActivityIndicator />;
+      }
+    }
 
     if (this.state.viewMode === "portrait") {
       headingText = (
@@ -213,18 +232,7 @@ class AuthScreen extends Component {
               </View>
             </View>
           </TouchableWithoutFeedback>
-          <ButtonWithBackground
-            color="#29aaf4"
-            onPress={this.loginHandler}
-            disabled={
-              (!this.state.controls.confirmPassword.valid &&
-                this.state.authMode === "signup") ||
-              !this.state.controls.email.valid ||
-              !this.state.controls.password.valid
-            }
-          >
-            Submit
-          </ButtonWithBackground>
+          {submitButton}
         </KeyboardAvoidingView>
       </ImageBackground>
     );
@@ -264,6 +272,12 @@ const styles = StyleSheet.create({
   }
 });
 
+const mapStateToProps = state => {
+  return {
+    isLoading: state.ui.isLoading
+  };
+};
+
 const mapDispatchToProps = dispatch => {
   return {
     onLogin: authData => dispatch(tryAuth(authData))
@@ -271,6 +285,6 @@ const mapDispatchToProps = dispatch => {
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(AuthScreen);
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,42 @@
-import {TRY_AUTH} from "./actionTypes";
+import { uiStartLoading, uiStopLoading } from "./ui";
+import startMainTabs from "../../screens/MainTabs/startMainTabs";
 
-export const tryAuth = (authData) => {
-  return {
-      type: TRY_AUTH,
-      authData: authData
-  }
+export const tryAuth = authData => {
+  return dispatch => {
+    dispatch(authSignup(authData));
+  };
+};
+
+export const authSignup = authData => {
+  return dispatch => {
+    dispatch(uiStartLoading);
+    fetch(
+      "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyC1k-HBwuAuheocLGay_jVqiOR7BmIDT7U",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: authData.email,
+          password: authData.password,
+          returnSecureToken: true
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    )
+      .catch(err => {
+        console.log(err);
+        alert("Authentication failed, please try again!");
+        dispatch(uiStopLoading());
+      })
+      .then(res => res.json())
+      .then(parsedRes => {
+        dispatch(uiStopLoading());
+        if (parsedRes.error) {
+          alert("Authentication failed, please try again!");
+        } else {
+          startMainTabs();
+        }
+      });
+  };
 };
