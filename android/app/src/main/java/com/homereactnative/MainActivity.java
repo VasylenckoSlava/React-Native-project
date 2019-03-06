81fb60f214796690c99f3cafca22be8a06ba1d7c
Nikita S <nikita.subbotin@itomy.ch>
2019-03-06 17:26:34 +0200

    Added spalsh screnn to ios and android platform

--- android/app/src/main/java/com/homereactnative/MainActivity.java
+++ android/app/src/main/java/com/homereactnative/MainActivity.java
@@ -1,10 +1,32 @@
 package com.homereactnative;
 
 import com.reactnativenavigation.controllers.SplashActivity;
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
+
 import com.imagepicker.permissions.OnImagePickerPermissionsCallback; // <- add this import
 import com.facebook.react.modules.core.PermissionListener; // <- add this import
 
 public class MainActivity extends SplashActivity implements OnImagePickerPermissionsCallback {
+  @Override
+  public LinearLayout createSplashLayout() {
+    LinearLayout view = new LinearLayout(this);
+    TextView textView = new TextView(this);
+
+    view.setBackgroundColor(Color.parseColor("#521751"));
+    view.setGravity(Gravity.CENTER);
+
+    textView.setTextColor(Color.parseColor("#fa923f"));
+    textView.setText("Itomych prodge");
+    textView.setGravity(Gravity.CENTER);
+    textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+    view.addView(textView);
+    return view;
+  }
   private PermissionListener listener; // <- add this attribute
 
     // Your methods here
@@ -26,4 +48,5 @@ public class MainActivity extends SplashActivity implements OnImagePickerPermiss
       }
       super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
+
 }
