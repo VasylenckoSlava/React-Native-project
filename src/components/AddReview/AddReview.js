4b84707119ef8e1be769a886c88d7920bf0fd75b
Nikita S <nikita.subbotin@itomy.ch>
2019-03-18 16:38:02 +0200

    Connected Place API Firebase to the project

--- src/components/AddReview/AddReview.js
+++ src/components/AddReview/AddReview.js
@@ -1,19 +1,79 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput
+} from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 class AddReview extends Component {
+  state = {
+    name: "",
+    rating: 0,
+    comment: "",
+    review: ""
+  };
+
+  submitReview = () => {
+    fetch(
+      "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyAhDNARMjr_D3PdHS-e_Nj8Abrk4Xplcrk"
+    ).then(response =>console.log(response.json()))
+  };
   close = () => {
     this.props.navigator.dismissModal();
   };
   render() {
     return (
-      <View style={styles.root}>
-        <TouchableOpacity style={styles.button} onPress={this.close}>
-          <Icon name="ios-close" size={30} color="#0066CC" />
-        </TouchableOpacity>
-        <Text style={styles.addReview}>Add Review</Text>
-      </View>
+      <KeyboardAwareScrollView style={{ flex: 1, backgroundColor: "#FFF" }}>
+        <View style={styles.root}>
+          <TouchableOpacity style={styles.button} onPress={this.close}>
+            <Icon name="ios-close" size={30} color="#0066CC" />
+          </TouchableOpacity>
+          <Text style={styles.addReview}>Add Review</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Name (optional)"
+            value={this.state.name}
+            onChangeText={name => this.setState({ name })}
+          />
+          <Text style={styles.rating}>Your Rating:</Text>
+          <View style={styles.stars}>
+            {[1, 2, 3, 4, 5].map(i => {
+              return (
+                <TouchableOpacity
+                  onPress={() => this.setState({ rating: i })}
+                  style={styles.startButton}
+                  key={i}
+                >
+                  <Icon
+                    name={"ios-star"}
+                    color={this.state.rating >= i ? "#FFD64C" : "#CCCCCC"}
+                    size={40}
+                  />
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+          <TextInput
+            style={[styles.input, { height: 100 }]}
+            placeholder="Review"
+            value={this.state.review}
+            onChangeText={review => this.setState({ review })}
+            multiline={true}
+            numberOfLines={5}
+          />
+
+          <TouchableOpacity
+            style={styles.submitButton}
+            onPress={this.submitReview}
+          >
+            <Text style={styles.submitButtonText}>Submit Review</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAwareScrollView>
     );
   }
 }
@@ -32,6 +92,41 @@ const styles = StyleSheet.create({
     color: "#444",
     textAlign: "center",
     margin: 20
+  },
+  input: {
+    padding: 10,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 3
+  },
+  rating: {
+    fontSize: 20,
+    color: "grey",
+    textAlign: "center",
+    marginVertical: 40
+  },
+  stars: {
+    marginBottom: 80,
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  startButton: {
+    padding: 5
+  },
+  submitButton: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "#0066cc",
+    borderRadius: 4,
+    marginVertical: 10,
+    marginHorizontal: 20
+  },
+  submitButtonText: {
+    fontSize: 18,
+    color: "#ffffff",
+    textAlign: "center"
   }
 });
 
